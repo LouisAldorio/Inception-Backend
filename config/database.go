03dbe8e13fcbea69068374c5418373d80c6ea82d
louisaldorio <louisaldorio@gmail.com>
2020-11-07 15:07:59 +0700

    fix gorm

--- config/database.go
+++ config/database.go
@@ -5,24 +5,22 @@ import (
 	"log"
 	"os"
 
-	"myapp/logger"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
-var dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
+var database_setting_main = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?multiStatements=true", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
 
-//ConnectDB connect GO to database
 func ConnectDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.InitLog(),
-	})
+	db, err := gorm.Open("mysql", database_setting_main)
 
 	if err != nil {
 		log.Print(err)
 		panic("Failed to connect database")
 	}
+
+	db.LogMode(true)
+	db.SingularTable(true)
 
 	return db
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.13
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/vektah/gqlparser/v2 v2.1.0
+	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.5
 )
